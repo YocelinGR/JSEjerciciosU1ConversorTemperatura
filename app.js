d92d4fcc778384496b3e45226db11e071d234e41
YocelinGR <garcia_romero.y@hotmail.com>
2018-06-03 22:23:31 -0500

    JS Unidad 1 ejercicio Conversor Temperatura

---
+++ app.js
@@ -0,0 +1,5 @@
+var tempCelcius= parseInt(prompt('Ingresa la temperatura en grados Celcius'));
+//Convirtiendo temperatura a grados Farenheit
+var tempFarenheit=(tempCelcius*1.8)+32;
+//Mostrando temperatura Farenheit
+document.write("La temperatura en grados Celcius es: "+tempCelcius+" La temperatura en grados Farenheit es: "+tempFarenheit);
